a31d23f492086aec51d4078570276b4422b45ae6
Sadwalps <sadwalps2255@gmail.com>
2026-01-16 22:47:54 +0530

    Register form

--- src/pages/AdminAuthentication.jsx
+++ src/pages/AdminAuthentication.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function AdminAuthentication({adminSignup}) {
+    const [adminDetails, setAdminDetails] = useState({
+        adminname:"",
+        email:"",
+        password:""
+    })
+    console.log(adminDetails);
+const handleRegister = async()=>{
+    const {adminname, email, password} = adminDetails
+    console.log(adminname, email, password);
+    if(!adminname || !email || !password){
+        alert(`Fill the form completely`)
+    }else{
+        alert(`Success`)
+    }
+    
+}
+
+    
     return (
         <>
             <div id='loginsignup' className='d-flex justify-content-center align-items-center'>
@@ -15,13 +33,13 @@ function AdminAuthentication({adminSignup}) {
                               {!adminSignup  ?<h1 className='text-light' style={{ fontWeight: "bold" }}>Login in Here</h1>:
                                 <h1 className='text-light' style={{ fontWeight: "bold" }}>Sign up Here</h1>}
 
-                               {adminSignup&& <input type="text" placeholder='Username' className='form-control p-2 mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />}
-                                <input type="email" placeholder='Email id' className='form-control p-2 mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />
-                                <input type="password" placeholder='Password' className='form-control p-2  mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />
+                               {adminSignup&& <input value={adminDetails.adminname} onChange={(e)=>setAdminDetails({...adminDetails,adminname:e.target.value})} type="text" placeholder='Name' className='form-control p-2 mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />}
+                                <input value={adminDetails.email} onChange={(e)=>setAdminDetails({...adminDetails,email:e.target.value})} type="email" placeholder='Email id' className='form-control p-2 mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />
+                                <input value={adminDetails.password} onChange={(e)=>setAdminDetails({...adminDetails,password:e.target.value})} type="password" placeholder='Password' className='form-control p-2  mt-lg-3 mt-2' style={{ borderRadius: "20px" }} />
 
                               {!adminSignup  ?<button type='button' className='btn btn-success p-2 w-100  mt-lg-3 mt-2' style={{ borderRadius: "20px" }} >LOGIN</button>:
 
-                                <button type='button' className='btn btn-primary p-2 w-100  mt-lg-3 mt-2' style={{ borderRadius: "20px" }} >SIGN UP</button>}
+                                <button onClick={handleRegister} type='button' className='btn btn-primary p-2 w-100  mt-lg-3 mt-2' style={{ borderRadius: "20px" }} >SIGN UP</button>}
 
                                 {adminSignup?<p className='mt-3 text-white'>Already a user? click here to <Link to={'/adminLogin'}>login</Link> </p>:
                                 <p className='mt-3 text-white'>Not a user? click here to <Link to={'/adminSignup'}>signup</Link> </p>}
