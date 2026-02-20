76ac244ae632b3441e8dde1941cb14667a2a6798
Sadwalps <sadwalps2255@gmail.com>
2026-02-20 19:55:41 +0530

    chats page updated

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,51 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function Chats() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [token, setToken] = useState("")
+  console.log(token);
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      setToken(sessionStorage.getItem("token"))
+    }
+  }, [])
+
   return (
     <>
       <Header />
       <div style={{ minHeight: "67vh" }}>
+        {token ? <div>
+          <div className='container-fluid text-center'>
+            <button onClick={handleShow} className='btn text-light btn-lg rounded-circle fs-3 mt-3 chatdivandbtn'>
+              <FontAwesomeIcon icon={faPlus} style={{ fontWeight: "bold" }} />
+            </button>
 
-        <div className='container-fluid text-center'>
-          <button className='btn btn-primary btn-lg rounded-circle fs-3 mt-3'>
-            <FontAwesomeIcon icon={faPlus} style={{ fontWeight: "bold" }} />
-          </button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title className='text-primary' style={{ fontWeight: "bold" }}>Add chats</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <textarea name="" id="" className='bg-primary form-control py-1 my-2 text-center rounded' placeholder='****' style={{ fontWeight: "bold" }}>
 
-          
-          <div className="row">
-            <div className="col-lg-2 col-md-2 col-sm-2 col-12"></div>
-            <div className="col-lg-8 col-md-8 col-sm-8 col-12">
-              <div className='w-100 mt-2 rounded bg-primary  text-center text-light p-1'>
-                <h4 className='pt-3' style={{ fontWeight: "bold" }}>jfhsdjkhkdjkhdsfkhgjkdhf</h4>
-                <div className='d-flex justify-content-between'>
-                  <button className='btn btn-light rounded text-primary'>
-                    <FontAwesomeIcon icon={faPenToSquare} className=' fs-4' />
-                  </button>
-                  <button className='btn btn-light text-danger rounded'>
-                    <FontAwesomeIcon icon={faTrash} className='fs-4' />
-                  </button>
+                </textarea>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="danger" onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Add
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            <div className="row mt-3">
+              <div className="col-lg-2 col-md-2 col-sm-2 col-12"></div>
+              <div className="col-lg-8 col-md-8 col-sm-8 col-12">
+                <div className='w-100 mt-2 rounded   text-center text-light p-1 chatdivandbtn' >
+                  <h4 className='pt-3' style={{ fontWeight: "bold" }}>jfhsdjkhkdjkhdsfkhgjkdhf</h4>
+                  <div className='d-flex justify-content-between'>
+                    <button className='btn btn-light rounded text-primary'>
+                      <FontAwesomeIcon icon={faPenToSquare} className=' fs-4' />
+                    </button>
+                    <button className='btn btn-light text-danger rounded'>
+                      <FontAwesomeIcon icon={faTrash} className='fs-4' />
+                    </button>
 
+                  </div>
                 </div>
+
               </div>
-
+              <div className="col-lg-2 col-md-2 col-sm-2 col-12"></div>
             </div>
-            <div className="col-lg-2 col-md-2 col-sm-2 col-12"></div>
           </div>
-        </div>
 
-        <div className='container-fluid ' style={{ height: "65vh" }}>
-          <div className="row">
-            <div className="col-3"></div>
-            <div className="col-6  text-info mt-5 pt-5  d-flex flex-column justify-content-center align-items-center">
-              <img src="https://www.creativefabrica.com/wp-content/uploads/2023/10/26/Empty-meal-tray-Cartoon-kicthen-contain-Graphics-82559752-1.png" alt="" style={{ height: "200px" }} />
-              <h1 className='text-center ' style={{ fontWeight: "bold" }}> chatbox is empty!!!</h1>
+          <div className='container-fluid ' style={{ height: "65vh" }}>
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6  text-info mt-5 pt-5  d-flex flex-column justify-content-center align-items-center">
+                <img src="https://www.creativefabrica.com/wp-content/uploads/2023/10/26/Empty-meal-tray-Cartoon-kicthen-contain-Graphics-82559752-1.png" alt="" style={{ height: "200px" }} />
+                <h1 className='text-center ' style={{ fontWeight: "bold" }}> chatbox is empty!!!</h1>
+              </div>
+              <div className="col-3"></div>
             </div>
-            <div className="col-3"></div>
           </div>
-        </div>
+        </div> :
+
+
+
+          <div className='container-fluid ' style={{ height: "65vh" }}>
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6  text-info mt-5 pt-5  d-flex flex-column justify-content-center align-items-center">
+                <img src="https://cdn-icons-png.flaticon.com/512/9693/9693192.png" alt="" style={{ height: "200px" }} />
+                <h1 className='text-center ' style={{ fontWeight: "bold" }}> Please Login!!!</h1>
+              </div>
+              <div className="col-3"></div>
+            </div>
+          </div>}
+
       </div>
       <Footer />
     </>
