0083c8056fadc9d680001ec38587ae11a43c7033
Sadwalps <sadwalps2255@gmail.com>
2026-04-17 21:27:35 +0530

    modified

--- src/pages/AdminRecipes2.jsx
+++ src/pages/AdminRecipes2.jsx
@@ -7,30 +7,30 @@ import Footer from '../components/Footer'
 import { SingleUsersRecipeContext } from '../context/ContextShare'
 
 function AdminRecipes2() {
-    const{singleUsersRecipe} = useContext(SingleUsersRecipeContext)
+    const { singleUsersRecipe } = useContext(SingleUsersRecipeContext)
     console.log(singleUsersRecipe);
     const [recipedetails, setRecipedetails] = useState([])
     console.log(recipedetails);
-    
-    
+
+
     const [uid, setUid] = useState("")
-console.log(uid);
+    console.log(uid);
 
-  useEffect(()=>{
-    if(sessionStorage.getItem("uid")){
-    setUid(sessionStorage.getItem("uid"))
-    }
-  })
+    useEffect(() => {
+        if (sessionStorage.getItem("uid")) {
+            setUid(sessionStorage.getItem("uid"))
+        }
+    })
 
-  useEffect(()=>{
-    
-  },[singleUsersRecipe])
+    useEffect(() => {
 
-  
- 
-  return (
-    <>
-      <div className='d-flex justify-content-between align-items-center px-3 bg-dark' style={{ minHeight: "10vh" }}>
+    }, [singleUsersRecipe])
+
+
+
+    return (
+        <>
+            <div className='d-flex justify-content-between align-items-center px-3 bg-dark' style={{ minHeight: "10vh" }}>
 
                 <div className='d-flex justify-content-center align-items-center gap-3 w-25' style={{ minHeight: "10vh" }}>
                     <Link to={'/admin'}>    <img className='' src="https://cdn-icons-png.freepik.com/512/10840/10840480.png" alt="app img" style={{ height: "45", width: "45px" }} /></Link>
@@ -55,12 +55,12 @@ console.log(uid);
 
 
             {/* recipes card section */}
-           {singleUsersRecipe ? <div className='container-fluid mt-lg-5 mt-3'>
+            {singleUsersRecipe ? <div className='container-fluid mt-lg-5 mt-3'>
                 <div className="row">
-                   {singleUsersRecipe?.map((item )=>( <div className="col-md-6 mt-4">
-                       <div className="row">
+                    {singleUsersRecipe?.map((item) => (<div className="col-md-6 mt-4">
+                        <div className="row">
                             <div className="col-md-2"></div>
-                             <div className="col-md-8">
+                            <div className="col-md-8">
                                 <Card className='border-0' style={{ width: '100%' }}>
                                     <Card.Img variant="top" src='https://tse1.mm.bing.net/th/id/OIP.-OeEPP-2XySfb7SXHYAR9gHaFj?rs=1&pid=ImgDetMain&o=7&rm=3' />
                                     <Card.Body>
@@ -73,7 +73,7 @@ console.log(uid);
                                         <h5 className='mb-2'>item?.category
 
                                         </h5>
-                                        <Button  variant="danger clas
+                                        <Button variant="danger clas
                                         rounded " style={{ fontWeight: "bold" }}><FontAwesomeIcon icon={faTrash} style={{ color: "white", marginRight: "7px" }} />Remove</Button>
                                     </Card.Body>
                                 </Card>
@@ -97,8 +97,8 @@ console.log(uid);
                     </div>
                 </div>}
             <Footer />
-    </>
-  )
+        </>
+    )
 }
 
 export default AdminRecipes2
